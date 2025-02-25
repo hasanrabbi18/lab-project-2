a91f01b91583bffedc163538ee76cc9711e99075
hasanrabbi18 <rabbi15-4890@diu.edu.bd>
2025-02-25 12:19:47 +0600

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,66 +11,15 @@ class LabClass05 extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Basic Flutter UI-02',
-      home: Scaffold(bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon( Icons.man)
-              ),BottomNavigationBarItem(
-              icon: Icon( Icons.woman)
-              ),
-              BottomNavigationBarItem(
-              icon: Icon( Icons.gif)
-              ),
-          ],
-        
-          ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Row(
-                  mainAxisAlignment:MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(
-                      Icons.home,
-                      size: 60,
-                      color: Color.fromARGB(228, 7, 7, 237),
-                    ),
-                    TextButton(
-                      onPressed:(){} , 
-                      child:Icon(Icons.close)),
-                  ],
-
-              ),
-              ),
-              ListTile(
-                leading: Icon(Icons.image),
-                title: Text("Image"),
-                onTap: () {},
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text("Settings"),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: Text("Basic Flutter UI-02"),
-          backgroundColor: const Color.fromARGB(255, 21, 59, 224),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [
-            CachedNetworkImage(
-              imageUrl: "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-              Image.network(
-              "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            ),
-          ],
-        ),
+      home: Scaffold(
+        drawer: Drawer(),
+      appBar: AppBar(
       ),
+        body:AppBar()
+      
+      
+      
+      )
     );
   }
 }
